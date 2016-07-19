d2011a3179c65f07be3d58eace6be6f3ecee6ceb
Artsiom <Artsiomxxx@gmail.com>
2016-07-19 17:55:47 +0300

    add the Task01(Fibonacci).

---
+++ Task01/FibonacciGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    public class FibonacciGenerator
+    {
+        /// <summary>
+        /// A method for generate Fibonacci numbers.
+        /// </summary>
+        /// <param name="value">The number of Fibonacci numbers.</param>
+        /// <returns>The variable type of IEnumerable.</returns>
+        public static IEnumerable<int> GetFibonacciNumber (int value)
+        {
+            if(value <= 0)
+                throw new ArgumentException();
+
+            int lhs = 0;
+            int rhs = 1;
+            int temp;
+
+            yield return 0; 
+
+            for (int i = 0; i < value - 1; i++)
+            {
+                yield return rhs;
+                temp = rhs;
+                rhs += lhs;
+                lhs = temp;
+            }
+        }
+
+    }
+}
